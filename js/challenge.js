563b2fe01ee7c02aa0c5220356ec7207b5d50bf7
stackguardian <mferdous98@aol.com>
2020-09-16 14:27:44 -0400

    likes

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,6 @@
 
 const timeControl = () => {
+    
 
     const incrementTime = () => {
         const counter = document.querySelector('#counter').innerText
@@ -17,8 +18,35 @@ const timeControl = () => {
         counterHeader.textContent = newScore
     }
 
+    const likeNumber = () => {
+        const likesUl = document.querySelector('.likes')
+
+        const currentNumber = document.querySelector('#counter').textContent
+        const numberLi = document.createElement('li')
+        numberLi.innerText = currentNumber
+
+        if (likes == null) {
+            let likes = numberLi.dataset.likes = 1
+         }
+         else {
+             likes = parseInt(likes) + 1 
+         }
+
+        likesUl.append(numberLi)
+
+        // let numberOfLikes = 0
+        // numberOfLikes++
+
+        // const likesUl = document.querySelector('.likes')
+        // const likeLi = document.createElement('li')
+        // currentScore has been liked (number of times) time, add conditional for time(s)
+
+    }
+    
+
     setInterval(incrementTime, 1000)
 
+    const likeButton = document.querySelector('#heart')
     const plusButton = document.querySelector('#plus')
     const minusButton = document.querySelector('#minus')
 
@@ -30,11 +58,14 @@ const timeControl = () => {
     minusButton.addEventListener('click', (e) => {
         deincrementTime()
     })
+
+    likeButton.addEventListener('click', e =>{
+        likeNumber()
+    })
     
-
-
 }
 
 timeControl()
 
+// 3. As a user, I can 'like' an individual number of the counter. I should see count of the number of 'likes' associated with that number.
 
